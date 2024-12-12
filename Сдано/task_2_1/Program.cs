799cb26d906ece7e5a5937da1b631d4362e0ff89
FlashAnton <safronov-ai@mail.ru>
2024-12-12 21:15:16 +0300

    Зачтено 2

---
+++ Сдано/task_2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace task_2_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string cnst1 = ",";
+            int cnst2 = 1;
+            int cnst3 = 2;
+            int cnst4 = 3;
+            string cnst5 = "Богу";
+            string cnst6 = "с";
+            string cnst7 = "всем";
+            Console.WriteLine("Выводимое константное сообщение...");
+            Console.WriteLine("{1} поклон {4}{0} {2} хозяину {5} хозяйкой{0} {3} добрым людям", cnst1, cnst2, cnst3, cnst4, cnst5, cnst6, cnst7);
+            Console.WriteLine("Нажмите любую клавишу для завершения");
+            Console.ReadKey(true);
+        }
+    }
+}
+
---
+++ Сдано/task_2_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Тр_пинка, зап__х, жур__вли, д__лёкая, _сенние, схв__тил, д_лина";
+            string result = text
+           .Replace("__", "/")
+           .Replace("_", "o")
+           .Replace("/", "a");
+            Console.WriteLine("Исходная строка: " + text);
+            Console.WriteLine("Результат: " + result);
+            Console.ReadKey(true);
+        }
+
+    }
+}
