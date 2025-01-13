58390be9743c3ce2ac2a86c776f0111dbf4beea7
lis'ka <146330993+Dimag11@users.noreply.github.com>
2025-01-13 18:20:13 +0300

    Add files via upload

---
+++ Zadanie9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Zadanie9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите длину массива n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            
+            double[] array = new double[n];
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Элемент {i + 1}: ");
+                array[i] = double.Parse(Console.ReadLine());
+            }
+
+            
+            double[] oddNumbers = array.Where(x => x % 2 != 0).ToArray();
+
+            
+            if (oddNumbers.Length == 0)
+            {
+                Console.WriteLine("\nВ массиве нет нечётных чисел.");
+            }
+            else
+            {
+                
+                Array.Sort(oddNumbers);
+
+                
+                Console.WriteLine("\nМассив из нечётных чисел, отсортированный по возрастанию:");
+                foreach (var num in oddNumbers)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
+    
