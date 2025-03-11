2ae2a4a5ef37a6972018c95ea93c54a8c5275593
lis'ka <146330993+Dimag11@users.noreply.github.com>
2025-03-12 00:16:20 +0300

    Add files via upload

--- Zadanie7/Program.cs
+++ Zadanie7/Program.cs
@@ -1,76 +1,79 @@
 ﻿using System;
 
-
 namespace Zadanie7
 {
     internal class Program
     {
-        // Функция для вычисления кубического корня вручную
-        static double CubeRoot(double value)
-        {
-            if (value >= 0)
-                return Math.Pow(value, 1.0 / 3.0); // Обычный корень для положительных
-            else
-                return -Math.Pow(Math.Abs(value), 1.0 / 3.0); // Корень для отрицательных
-        }
-    
         static void Main()
-
         {
-            Console.WriteLine("Введите значения для a,B,XO,Xk,delX,n соответственно");
+            Console.WriteLine("Введите значения для a, B, X0, XK, delX, n соответственно");
+
             double a = 0, b = 0, X0 = 0, XK = 0, delX = 0, eps = 0.001, n = 0;
+
+            
             while (true)
             {
                 Console.Write("Введите значение a: ");
                 if (double.TryParse(Console.ReadLine(), out a))
                     break;
-                else { Console.WriteLine("Ошибка! Введите числа! Попробуйте ещё раз"); }
+                else { Console.WriteLine("Ошибка! Введите число!"); }
             }
             while (true)
             {
                 Console.Write("Введите значение B: ");
                 if (double.TryParse(Console.ReadLine(), out b))
                     break;
-                else { Console.WriteLine("Ошибка! Введите числа! Попробуйте ещё раз"); }
+                else { Console.WriteLine("Ошибка! Введите число!"); }
             }
             while (true)
             {
                 Console.Write("Введите значение X0: ");
                 if (double.TryParse(Console.ReadLine(), out X0))
                     break;
-                else { Console.WriteLine("Ошибка! Введите числа! Попробуйте ещё раз"); }
+                else { Console.WriteLine("Ошибка! Введите число!"); }
             }
             while (true)
             {
                 Console.Write("Введите значение XK: ");
                 if (double.TryParse(Console.ReadLine(), out XK))
                     break;
-                else { Console.WriteLine("Ошибка! Введите числа! Попробуйте ещё раз"); }
+                else { Console.WriteLine("Ошибка! Введите число!"); }
             }
             while (true)
             {
                 Console.Write("Введите значение delX: ");
                 if (double.TryParse(Console.ReadLine(), out delX))
                     break;
-                else { Console.WriteLine("Ошибка! Введите числа! Попробуйте ещё раз"); }
+                else { Console.WriteLine("Ошибка! Введите число!"); }
             }
             while (true)
             {
                 Console.Write("Введите значение n: ");
                 if (double.TryParse(Console.ReadLine(), out n))
                     break;
-                else { Console.WriteLine("Ошибка! Введите числа! Попробуйте ещё раз"); }
-
+                else { Console.WriteLine("Ошибка! Введите число!"); }
             }
 
+            Console.WriteLine("\nРезультаты вычислений:");
+
+           
             while (X0 <= XK + eps)
             {
-                double denominatorValue = Math.Tan(CubeRoot(Math.Pow(a, 2) + b - n));
+                // Расчёт кубического корня вручную
+                double valueForRoot = Math.Pow(a, 2) + b - n;
+                double cubeRoot;
+                if (valueForRoot >= 0)
+                    cubeRoot = Math.Pow(valueForRoot, 1.0 / 3.0); // Корень для положительных чисел
+                else
+                    cubeRoot = -Math.Pow(Math.Abs(valueForRoot), 1.0 / 3.0); // Корень для отрицательных чисел
+
+                // Остальные вычисления
+                double denominatorValue = Math.Tan(cubeRoot);
                 double numerator = 0.5 * Math.Cos(a * Math.Pow(X0, a)) + b;
 
                 double j = (numerator / denominatorValue) + Math.Pow(a, 3) * Math.Exp(X0);
 
-                Console.WriteLine($"В точке x={X0:F2} арифметическое выражение = {j:F4}");
+                Console.WriteLine($"В точке x={X0:F2} функция равна = {j:F4}");
 
                 X0 += delX;
             }
